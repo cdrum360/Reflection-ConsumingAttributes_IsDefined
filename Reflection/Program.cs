5c7a1320e9ab4c50d377f9931bb7895ff1041dd5
Charles W. Drummond Jr <cdrum360@live.com>
2017-05-13 13:53:15 -0400

    Add project files.

---
+++ Reflection/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Reflection
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public sealed class ReviewCommentAttribute : Attribute
+    {
+        public string Description { get; set; }
+        public string VersionNumber { get; set; }
+        public string ReviewerId { get; set; }
+        
+        public ReviewCommentAttribute(string desc, string ver)
+        {
+            Description = desc;
+            VersionNumber = ver;
+        }
+    }
+        
+    [ReviewComment("Check it out", "3.0")]
+    class MyClass { }   
+  
+    public class Program
+    {
+        static void Main()
+        {
+            MyClass mc = new MyClass();         // Create an instance of the class.
+            Type t = mc.GetType();              // Get the Type object from the instance.
+            bool isDefined = t.IsDefined(typeof(ReviewCommentAttribute), false);     // Check the Type for the attribute.
+
+            if (isDefined)
+            Console.WriteLine("ReviewComment is applied to type {0}", t.Name);
+
+            Console.ReadLine();
+
+        }
+     }
+
+}
+
